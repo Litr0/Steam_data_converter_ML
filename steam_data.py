fc317a3f02357e4b72977fc11e2dc660b9868b01
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-10-31 10:29:46 +0100

    Refactor main_4 function to use updated steam reviews dataset and adjust unique counts

--- steam_data.py
+++ steam_data.py
@@ -295,24 +295,21 @@ def main_3():
 
 # See how many users and games are in the dataset
 def main_4():
-    steam_reviews = pd.read_csv('data/steam_reviews.csv')
-
-    # Filter out non-english reviews
-    steam_reviews_english = steam_reviews[(steam_reviews["language"] == "english")]
+    steam_reviews = pd.read_csv('data/steam_reviews_all_2017_new.csv')
 
     # Print the size of the dataset
-    print(f"Number of rows in the dataset: {steam_reviews_english.shape[0]}")
+    print(f"Number of rows in the dataset: {steam_reviews.shape[0]}")
 
     # Get the number of unique users
-    unique_users = steam_reviews_english['author.steamid'].nunique()
+    unique_users = steam_reviews['author_id'].nunique()
     print(f"Number of unique users: {unique_users}")
 
     # Get the number of unique games
-    unique_games = steam_reviews_english['app_id'].nunique()
+    unique_games = steam_reviews['app_id'].nunique()
     print(f"Number of unique games: {unique_games}")
 
     # Get the number of unique reviews
-    unique_reviews = steam_reviews_english['review_id'].nunique()
+    unique_reviews = steam_reviews['review_id'].nunique()
     print(f"Number of unique reviews: {unique_reviews}")
 
 
