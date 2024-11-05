dc142d04b6bac304493e5aa6718dc671151b9fea
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-11-05 10:41:24 +0100

    Expand game selection to include seven titles in main_10 function

--- steam_data.py
+++ steam_data.py
@@ -541,7 +541,9 @@ def main_10():
     df = df[(df['timestamp'] >= 1483228800) & (df['timestamp'] <= 1514764799)]
 
     # Plot the number of the reviews of 7 games
-    games = ['Grand Theft Auto V', 'Firewatch', 'Sonic Mania']
+    games = ['Grand Theft Auto V', 'Firewatch', 'Sonic Mania', 
+             'PLAYERUNKNOWN\'S BATTLEGROUNDS', 'Doki Doki Literature Club', 
+             'Tom Clancy\'s Rainbow Six Siege', 'Rocket League']
     
     seven_games = df[df['app_name'].isin(games)]
 
