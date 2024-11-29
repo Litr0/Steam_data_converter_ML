fa1f6da6eef6d3a4a1e76ee7606ce716def2b101
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-11-29 13:34:19 +0100

    Enhance main_13 to print confirmation messages after saving modified data to multiple file paths

--- steam_data.py
+++ steam_data.py
@@ -664,8 +664,11 @@ def main_13(path = '/home/bigdama/projects/tgn/data/steam_2017_new.csv'):
     df = df.sort_values(by=['timestamp'], ascending=[True])
 
     df.to_csv('data/steam_2017_new_modified.csv', index=False)
+    print("Data saved to 'data/steam_2017_new_modified.csv'")
     df.to_csv('/home/bigdama/projects/tgn/data/steam_2017_new_modified.csv', index=False)
+    print("Data saved to '/home/bigdama/projects/tgn/data/steam_2017_new_modified.csv'")
     df.to_csv('/home/bigdama/projects/bidyn/data/steam_2017_new_modified.csv', index=False)
+    print("Data saved to '/home/bigdama/projects/bidyn/data/steam_2017_new_modified.csv'")
     
 if __name__ == "__main__":
     main_12()
