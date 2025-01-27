f660f719dd73ae50905344952f8a3152ef49a741
Litr0 <gonzalo.sobarzoa@gmail.com>
2025-01-27 11:17:08 +0100

    Add delay in main_17 function to improve output readability

--- steam_data.py
+++ steam_data.py
@@ -801,6 +801,7 @@ def main_17():
     print(f"Cos sim abusive: {cos_sim_abusive}")
     print(f"Mean sim abusive: {mean_sim_abusive}")
     print(f"Cos sim non abusive: {cos_sim_non_abusive}")
+    time.sleep(20)
     print(f"Mean sim non abusive: {mean_sim_non_abusive}")
 
 if __name__ == "__main__":
