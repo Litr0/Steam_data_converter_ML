56f41f8ba7a5adc61fdc8d67fcee1c54b2c52962
Litr0 <gonzalo.sobarzoa@gmail.com>
2025-01-28 10:55:51 +0100

    Add loading of user embeddings and their statistics in main_17

--- steam_data.py
+++ steam_data.py
@@ -787,6 +787,10 @@ def main_17():
 
     with open(path, "rb") as f:
         preds = pickle.load(f)
+        u_embs = preds['u_embs']
+        u_embs_np = preds['u_embs_np']
+        u_embs_abusive = preds['u_embs_abusive']
+        u_embs_non_abusive = preds['u_embs_non_abusive']
         mean_abusive = preds['mean_abusive']
         std_abusive = preds['std_abusive']
         mean_non_abusive = preds['mean_non_abusive']
