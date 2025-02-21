121ee856e438d26423c7f28b8bc6dae7ba399ca5
Litr0 <gonzalo.sobarzoa@gmail.com>
2025-02-21 11:11:29 +0100

    Fix batch index handling by converting to int for feature masks in main_19

--- steam_data.py
+++ steam_data.py
@@ -1129,16 +1129,13 @@ def main_19():
     new_test_feats = []
     for batch_idxs, group, side_name in batch_idxs_array:
         if side_name == 'u':
-            print(f"Batch idxs: {batch_idxs}")
-            print(f"Group: {group}")
-            print(f"Batcn idxs type: {type(batch_idxs)}")
             train_mask = u_train_mask[batch_idxs]
             val_mask = u_val_mask[batch_idxs]
             test_mask = u_test_mask[batch_idxs]
             labels = u_labels[batch_idxs]
-            train_feats_mask = train_feats[batch_idxs]
-            val_feats_mask = val_feats[batch_idxs]
-            test_feats_mask = test_feats[batch_idxs]
+            train_feats_mask = train_feats[batch_idxs.astype(int)]
+            val_feats_mask = val_feats[batch_idxs.astype(int)]
+            test_feats_mask = test_feats[batch_idxs.astype(int)]
             if group == 'train':
                 new_train_labels.extend(labels[train_mask].tolist())
                 new_train_feats.extend(train_feats_mask[train_mask]) 
