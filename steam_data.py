d52d0c0b75f5cfad5a1d444e8de37a27a0f20466
Litr0 <gonzalo.sobarzoa@gmail.com>
2025-02-21 10:53:57 +0100

    Refactor feature extraction in main_19 to simplify list extension for training, validation, and test sets

--- steam_data.py
+++ steam_data.py
@@ -1144,12 +1144,12 @@ def main_19():
             test_feats_mask = test_feats[batch_idxs]
             if group == 'train':
                 new_train_labels.extend(labels[train_mask].tolist())
-                new_train_feats.extend(train_feats_mask[train_mask].tolist()) 
+                new_train_feats.extend(train_feats_mask[train_mask]) 
             else:
                 new_val_labels.extend(labels[val_mask].tolist())
                 new_test_labels.extend(labels[test_mask].tolist())
-                new_val_feats.extend(val_feats_mask[val_mask].tolist())
-                new_test_feats.extend(test_feats_mask[test_mask].tolist())
+                new_val_feats.extend(val_feats_mask[val_mask])
+                new_test_feats.extend(test_feats_mask[test_mask])
     
     print("Number of new train labels:", len(new_train_labels))
     print("Number of train labels:", len(train_labels))
